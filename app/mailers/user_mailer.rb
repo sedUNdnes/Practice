4e665eee0c45ce38b7cbc1bbf3738e12829e1a06
Nicolas <nrestrepot@unal.edu.co>
2017-09-27 05:07:03 +0000

    Attachments example

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,6 +6,15 @@ class UserMailer < ApplicationMailer
     @url  = 'https://ataraxia-sedundnes.herokuapp.com/'
     mail(to: @user.email, subject: 'Welcome')
   end
-  
+  def welcome_email_with_attachments(user)
+    @user = user
+    @url  = 'https://ataraxia-sedundnes.herokuapp.com/'
+    @song = 'https://www.youtube.com/watch?v=-nFzr24qo1c'
+    # Mark this mail as no-spam
+    headers({'X-No-Spam' => 'True', 'In-Reply-To' => 'complaints@dontcare.com'})
+    # Attaching a beautiful cover from Vampire Weekend
+    attachments['vwws.jpg'] = File.read("#{Rails.root}/public/vwws.jpg")
+    mail(to: @user.email, subject: 'Look that cover!')
+  end
   
 end
