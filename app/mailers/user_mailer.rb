5528c1ae6a295a966e7317b854c06290ff7beb91
Nicolas <nrestrepot@unal.edu.co>
2017-09-27 01:50:34 +0000

    Initial configurations: New mailer created, Scaffold for testing, Welcome email edited

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,8 @@
+class UserMailer < ApplicationMailer
+    
+  def welcome_email(user)
+    @user = user
+    @url  = 'https://ataraxia-sedundnes.herokuapp.com/'
+    mail(to: @user.email, subject: 'Welcome')
+  end
+end
