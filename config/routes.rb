1b708e8f3b0c2d4ed88a9b73e176eeaa3f3420d1
Lizzy <ltenganah@unal.edu.co>
2017-09-29 03:46:43 +0000

    I18n configured

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  if Rails.env.development?
-    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+# scope "(:locale)", locale: /en|es/ do
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    if Rails.env.development?
+      mount LetterOpenerWeb::Engine, at: "/letter_opener"
+    end
+    #Route for scaffolding Users
+    resources :users
+    root 'users#index'
+    get 'multiple_recipients/send_to_all', to: 'multiple_recipients#send_to_all', as: :send_to_all
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    
   end
-  #Route for scaffolding Users
-  resources :users
-  root 'users#index'
-  get 'multiple_recipients/send_to_all', to: 'multiple_recipients#send_to_all', as: :send_to_all
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
 end
